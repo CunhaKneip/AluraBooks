09e3d2bd93fbc8bc7ccceb2b9c6e97439c100b50
Gustavo Cunha <102625805+CunhaKneip@users.noreply.github.com>
2023-07-03 13:06:03 -0300

    comentarios

--- Scripts/Script1.js
+++ Scripts/Script1.js
@@ -1,57 +1,13 @@
-function handleTabletMediaQuery(mediaQueryTablet) {
-    if (mediaQueryTablet.matches) {
-        const swiper = new Swiper('.swiper', {
-            spaceBetween: 0,
-            slidesPerView: 3,
-            centeredSlides: false,
-            loop: false,
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-            },
-        });
-    }
-}
 
-function handleMobileMediaQuery(mediaQueryMobile) {
-    if (mediaQueryMobile.matches) {
-        const swiper = new Swiper('.swiper', {
-            spaceBetween: 50,
-            slidesPerView: 'auto',
-            centeredSlides: true,
-            loop: false,
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-            },
-        });
-    }
-}
 
-function handleDesktopMediaQuery(mediaQueryDesktop) {
-    if (mediaQueryDesktop.matches) {
-        const swiper = new Swiper('.swiper', {
-            spaceBetween: 50,
-            slidesPerView: 'auto',
-            centeredSlides: true,
-            loop: false,
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-            },
-        });
-    }
-}
+//Declara uma variavel para comprovar a existencia ou nao do SWIPER
+let swiper = null; 
 
-const mediaQueryTablet = window.matchMedia("(min-width: 1024px)");
-const mediaQueryDesktop = window.matchMedia("(min-width: 1728px)");
-const mediaQueryMobile = window.matchMedia("(max-width: 767px )");
 
-let swiper = null; // Declare a variable to hold the Swiper instance
-
+//Quando chamada, essa função destroi o SWIPER caso ele já exista e cria outro atendendo as especificações para cada dispositivo
 function handleMediaQueryChange() {
     if (swiper !== null) {
-        swiper.destroy(); // Destroy the existing Swiper instance
+        swiper.destroy(); 
     }
 
     if (mediaQueryTablet.matches) {
@@ -89,26 +45,32 @@ function handleMediaQueryChange() {
         });
     }
 }
-
+//Chama a função uma primeira vez
 handleMediaQueryChange();
 
+//EventListener responsável por perceber a mudança no tamanho da tela e chamar a função handleMediaQueryChange, realizando assim a troca dos tipos de swiper
 mediaQueryTablet.addEventListener("change", handleMediaQueryChange);
 mediaQueryMobile.addEventListener("change", handleMediaQueryChange);
 mediaQueryDesktop.addEventListener("change", handleMediaQueryChange);
 
+
+
+
+// Código responsável por mudar a posição do quadro de categorias, 
+// foi necessário pois ele estava mudando de posição em certos tamanhos, agora ele segue o seu elemento "pai" e fica onde deve ficar
 function updatePosition() {
     var element1 = document.getElementById('element1');
     var element2 = document.getElementById('element2');
   
     var rect = element1.getBoundingClientRect();
-    var top = rect.bottom; /* Use rect.bottom to position element2 below element1 */
+    var top = rect.bottom; 
     var left = rect.left;
   
     element2.style.top = top + 'px';
     element2.style.left = left + 'px';
   }
   
-  // Call the updatePosition function initially and whenever the window is resized
+//EventListener que age quando o tamanho da tela é mudado
   updatePosition();
   window.addEventListener('resize', updatePosition);
   
